4c8c671eb92f5de680e1d2b3e5c0941361fbaa7e
garrettdavid1 <garrettdavid1@gmail.com>
2016-12-14 23:16:01 -0500

    Re-designed CookBooks Index View - Much neater now. No tacky designs.

--- SimpleKitchen/Scripts/CookBooksIndexScript.js
+++ SimpleKitchen/Scripts/CookBooksIndexScript.js
@@ -1,6 +1,12 @@
 ﻿$(document).ready(function () {
-    $(".cookbook-jumbotron").click(function () {
-        $(this).siblings(".recipe-container").toggleClass("hidden");
-        $(this).parent().closest("div").siblings(".cookbook-container").toggleClass("hidden");
+    $(".cookbook-name-button").click(function () {
+        var selector = ".cb-" + $(this).val().toString();
+        $(selector).toggleClass("hidden");
+    });
+    $("#show-all-recipes").click(function () {
+        $(".individual-recipe").removeClass("hidden");
+    });
+    $("#hide-all-recipes").click(function () {
+        $(".individual-recipe").addClass("hidden");
     });
 });
