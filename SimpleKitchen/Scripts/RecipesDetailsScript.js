13754ed1caf1f04d89b0e6fbbaf781777a5be109
garrettdavid1 <garrettdavid1@gmail.com>
2016-12-28 19:16:32 -0500

    Got rid of RecipesDetailsScript

--- SimpleKitchen/Scripts/RecipesDetailsScript.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $(function(){
-         if ($(".screen-size-indicator").css("font-size") === "2px") {
-             $(".yields-buffer").addClass("hidden");
-         } else {
-             $(".yields-buffer").removeClass("hidden");
-         }
-     })
-})
