80406b2a94651083c971cbe39dd55ab5d28f8eba
garrettdavid1 <garrettdavid1@gmail.com>
2017-01-21 19:14:28 -0500

    Removed delete-recipe & remove-recipe button from Public Recipes, and deleted unnecessary PublicRecipeDetailsScript and references to it.

--- SimpleKitchen/Scripts/PublicRecipesScript.js
+++ SimpleKitchen/Scripts/PublicRecipesScript.js
@@ -1,4 +1,6 @@
 ﻿$(document).ready(function () {
+    $(".remove-recipe").remove();
+    $(".delete-recipe").remove();
     $(".individual-recipe-container").removeClass("hidden");
     $(".hide-individual-recipe").click(function () {
         $(this).parent().parent().addClass("hidden");
